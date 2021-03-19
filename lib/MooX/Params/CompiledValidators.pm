20fff4a66f40f0c9f0a6ccedc71e314540cf94e3
Abe Timmerman <abeltje@cpan.org>
2021-03-19 05:08:02 +0100

    Autocommit for distribution MooX::Params::CompiledValidators 0.02 (same)

--- lib/MooX/Params/CompiledValidators.pm
+++ lib/MooX/Params/CompiledValidators.pm
@@ -1,7 +1,7 @@
 package MooX::Params::CompiledValidators;
 use Moo::Role;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 use Hash::Util 'lock_hash';
 use Params::ValidationCompiler 'validation_for';
